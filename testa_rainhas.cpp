76c0220a5e4c7525add42da3953cccfdc172d2c2
João Felipe Silva <93218068+JF3lip3@users.noreply.github.com>
2023-06-06 21:53:46 -0300

    teste 4

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -48,4 +48,17 @@ TEST_CASE("Teste de solução inválida do tabuleiro ou rainhas") {
     REQUIRE(verifica_soluc(board) == -1);
 }
 
+TEST_CASE("Teste de solução válida das 8 damas 2") {
+    std::vector<int> tab = {
+        1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0,
+        0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0
+    };
 
+    REQUIRE(verifica_soluc(tab) == 1);
+}
