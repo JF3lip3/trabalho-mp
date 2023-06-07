f5295bcf05285c184f704ba5b70cff299bc03704
João Felipe Silva <93218068+JF3lip3@users.noreply.github.com>
2023-06-06 22:34:06 -0300

    teste 30

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -436,3 +436,18 @@ TEST_CASE("15° Teste de validação do tab inválido: solução inválida") {
 
     REQUIRE(verifica_soluc(tabuleiro) == 0);
 }
+
+TEST_CASE("16° Teste de validação do tab inválido: solução inválida") {
+    std::vector<int> tabuleiro = {
+        0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 1, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1,
+        0, 1, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 0
+    };
+
+    REQUIRE(verifica_soluc(tabuleiro) == 0);
+}
