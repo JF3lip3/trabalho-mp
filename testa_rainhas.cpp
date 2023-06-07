758e58a76882461652045b951c181f8419cb8505
João Felipe Silva <93218068+JF3lip3@users.noreply.github.com>
2023-06-06 22:13:05 -0300

    teste 14

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -197,3 +197,18 @@ TEST_CASE("Decimo Teste de solução válida das 8 damas") {
 
     REQUIRE(verifica_soluc(tabuleiro) == 1);
 }
+
+TEST_CASE("Decimo segundo Teste de solução válida das 8 damas") {
+    std::vector<int> tabuleiro = {
+        0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0,
+        1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1,
+        0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 0, 0
+    };
+
+    REQUIRE(verifica_soluc(tabuleiro) == 1);
+}
